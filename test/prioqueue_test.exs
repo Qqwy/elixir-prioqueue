4ac87ebdcd7e0baa3dceb65eb3a2cfa6ef9fbf73
Dmitry Ledentsov <dmnled@gmail.com>
2019-12-17 15:18:07 +0100

    fixed :default_comparison_function spelling

--- test/prioqueue_test.exs
+++ test/prioqueue_test.exs
@@ -61,6 +61,23 @@ defmodule PrioqueueTest do
       assert (simple_prioqueue(unquote(impl)) |> Prioqueue.member?(1))
     end
 
+    test "#{impl} :default_comparison_function?" do
+      # temporarily set the cmp_inverse in the application
+      Application.put_env(:prioqueue, :default_comparison_function, &Prioqueue.Helper.cmp_inverse/2)
+
+      pqueue_inverse_app = Enum.into([1, 3, 2], Prioqueue.empty(implementation: unquote(impl)))
+
+      # restore the default
+      Application.put_env(:prioqueue, :default_comparison_function, &Prioqueue.Helper.cmp/2)
+
+      assert Enum.map(pqueue_inverse_app,fn x -> x end) == [3, 2, 1]
+    end
+
+    test "#{impl} cmp_fun?" do
+      pqueue_inverse = Enum.into([1, 3, 2], Prioqueue.empty(implementation: unquote(impl), cmp_fun: &Prioqueue.Helper.cmp_inverse/2))
+      assert Enum.map(pqueue_inverse,fn x -> x end) == [3, 2, 1]
+    end
+
   end
   def simple_prioqueue(impl) do
     Prioqueue.empty(implementation: impl)
